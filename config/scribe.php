8f3cdedc8b428e8cfbae4b0a81fa38002f85e8a6
Neni <felipe.dacs@gmail.com>
2024-09-24 01:26:04 -0300

    :zzz: usa somente make para o scribe

--- config/scribe.php
+++ config/scribe.php
@@ -192,7 +192,7 @@ INTRO
         // With API resources and transformers, Scribe tries to generate example models to use in your API responses.
         // By default, Scribe will try the model's factory, and if that fails, try fetching the first from the database.
         // You can reorder or remove strategies here.
-        'models_source' => ['factoryCreate', 'factoryMake', 'databaseFirst'],
+        'models_source' => ['factoryMake'],
     ],
 
     // The strategies Scribe will use to extract information about your routes at each stage.
